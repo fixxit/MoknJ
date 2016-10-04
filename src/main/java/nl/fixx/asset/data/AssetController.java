f07e26683502b4347ddf146bee86ce673a2f9aee
Colin <colin.deklerk@fixx.it>
2016-10-04 16:14:03 +0200

    wip

--- src/main/java/nl/fixx/asset/data/AssetController.java
+++ src/main/java/nl/fixx/asset/data/AssetController.java
@@ -12,27 +12,14 @@ import nl.fixx.asset.data.domain.Asset;
 import nl.fixx.asset.data.repository.AssetRepository;
 
 @RestController()
-public class GreetingController {
-
-    private static final String TEMPLATE = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+public class AssetController {
 
     @Autowired
     private AssetRepository assetRepository;
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World?") String name) {
-	return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
-    }
-
-    @RequestMapping("/testmongo")
+    @RequestMapping("/addAsset")
     public void testMongo(@RequestParam(value = "name", defaultValue = "") String name) {
-	assetRepository.deleteAll();
 	assetRepository.save(new Asset("Laptop", name));
-
-	for (Asset asset : assetRepository.findAll()) {
-	    System.out.println(asset.toString());
-	}
     }
     
     @RequestMapping("/showassets")
--- src/main/java/nl/fixx/asset/data/repository/AssetRepository.java
+++ src/main/java/nl/fixx/asset/data/repository/AssetRepository.java
@@ -2,7 +2,6 @@ package nl.fixx.asset.data.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import nl.fixx.asset.data.domain.Asset;
 
