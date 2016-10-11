aa16bb23585e28958924f0bd3838551e8c4d6186
Riaan Schoeman <adriaan.s.schoeman@gmail.com>
2016-10-11 15:01:07 +0200

    fix of prior commit.

--- src/main/java/nl/fixx/asset/data/Application.java
+++ src/main/java/nl/fixx/asset/data/Application.java
@@ -1,15 +1,21 @@
 package nl.fixx.asset.data;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+
 @SpringBootApplication
 @EnableGlobalMethodSecurity
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+	SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/nl/fixx/asset/data/AssetController.java
+++ src/main/java/nl/fixx/asset/data/AssetController.java
@@ -1,6 +1,8 @@
 package nl.fixx.asset.data;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
 import nl.fixx.asset.data.domain.Asset;
 import nl.fixx.asset.data.info.AssetResponse;
 import nl.fixx.asset.data.repository.AssetRepository;
@@ -8,35 +10,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin // added for cors, allow access from another web server
-@RestController()
+@RestController
 @RequestMapping(value = "/asset")
 public class AssetController {
 
     @Autowired
     private AssetRepository assetRepository;
 
-    @RequestMapping(value = "/addAsset", method = RequestMethod.POST)
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public AssetResponse addAsset(@RequestBody Asset asset) {
-	System.out.println(asset.toString());
-	AssetResponse assetResponse = new AssetResponse();
-	assetResponse.setAction("POST");
-	assetResponse.setMethod("/addAsset");
+        System.out.println(asset.toString());
+        AssetResponse assetResponse = new AssetResponse();
+        assetResponse.setAction("POST");
+        assetResponse.setMethod("/add");
 
-	try {
-	    Asset savedAsset = this.assetRepository.save(asset);
-	    assetResponse.setSuccess(true);
-	    assetResponse.setMessage(savedAsset.getId());
-	} catch (IllegalArgumentException ex) {
-	    assetResponse.setSuccess(false);
-	    assetResponse.setMessage(ex.getMessage());
-	}
+        try {
+            Asset savedAsset = this.assetRepository.save(asset);
+            assetResponse.setSuccess(true);
+            assetResponse.setMessage(savedAsset.getId());
+        } catch (IllegalArgumentException ex) {
+            assetResponse.setSuccess(false);
+            assetResponse.setMessage(ex.getMessage());
+        }
 
-	return assetResponse;
+        return assetResponse;
     }
 
-    @RequestMapping("/getAllAssets")
-    public List<Asset> getAllAssets() {
-	return assetRepository.findAll();
+    @GetMapping("/asset")
+    public Asset asset() throws Exception {
+        Asset asset = new Asset();
+        asset.setDescription("description");
+        asset.setName("name");
+        asset.setPrice(BigDecimal.ONE);
+        asset.setPurchaseDate(new Date());
+        return asset;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.POST)
+    public ArrayList<Asset> getAllAssets() {
+        ArrayList list = new ArrayList<Asset>();
+        list.addAll(assetRepository.findAll());
+        return list;
     }
 
 }
