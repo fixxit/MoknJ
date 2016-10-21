211c021d6c4b68ae0c075e239c1f8f3237a9df83
Colin <colin.deklerk@fixx.it>
2016-10-21 15:52:12 +0200

    fixed test structure

--- src/main/java/nl/fixx/asset/data/test/TestResourceRepository.java
+++ src/main/java/nl/fixx/asset/data/test/TestResourceRepository.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
+import nl.fixx.asset.data.builders.ResourceBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +19,7 @@ import nl.fixx.asset.data.domain.Resource;
 import nl.fixx.asset.data.repository.ResourceRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { MongoConfiguration.class })
+@ContextConfiguration(classes = {MongoConfiguration.class})
 public class TestResourceRepository {
 
     @Autowired
@@ -26,77 +27,78 @@ public class TestResourceRepository {
 
     @Before
     public void init() {
-	saveTest();
+        saveTest();
     }
 
     @Test
     public void saveTest() {
-	Resource resource = new Resource();
-	resource.setContactNumber("0644010101");
-	resource.setEmail("junit_test@fixx.it");
-	resource.setFirstName("Courage");
-	resource.setSurname("Cowardly Dog");
+        Resource resource = new ResourceBuilder()
+                .contactNumber("0644010101")
+                .email("junit_test@fixx.it")
+                .firstName("Courage")
+                .surname("Cowardly Dog")
+                .build();
 
-	Resource resourceRet = this.repository.save(resource);
+        Resource resourceRet = this.repository.save(resource);
 
-	assertNotNull(resourceRet.getId());
+        assertNotNull(resourceRet.getId());
     }
 
     @Test
     public void updateTest() {
-	Resource resource = this.repository.findByEmail("junit_test@fixx.it");
-	String id = resource.getId();
-	assertNotNull(resource);
+        Resource resource = this.repository.findByEmail("junit_test@fixx.it");
+        String id = resource.getId();
+        assertNotNull(resource);
 
-	// update
-	resource.setSurname("Uber Dog");
-	resource = this.repository.save(resource);
-	assertSame(id, resource.getId());
-	assertSame("Uber Dog", resource.getSurname());
+        // update
+        resource.setSurname("Uber Dog");
+        resource = this.repository.save(resource);
+        assertSame(id, resource.getId());
+        assertSame("Uber Dog", resource.getSurname());
 
-	// restore
-	resource.setSurname("Cowardly Dog");
-	resource = this.repository.save(resource);
-	assertSame(id, resource.getId());
-	assertSame("Cowardly Dog", resource.getSurname());
+        // restore
+        resource.setSurname("Cowardly Dog");
+        resource = this.repository.save(resource);
+        assertSame(id, resource.getId());
+        assertSame("Cowardly Dog", resource.getSurname());
     }
 
     @Test
     public void findByIdEmailTest() {
-	// first find without id
-	Resource resource = this.repository.findByEmail("junit_test@fixx.it");
-	assertNotNull(resource);
+        // first find without id
+        Resource resource = this.repository.findByEmail("junit_test@fixx.it");
+        assertNotNull(resource);
 
-	resource = this.repository.findById(resource.getId());
-	assertEquals("0644010101", resource.getContactNumber());
-	assertEquals("junit_test@fixx.it", resource.getEmail());
-	assertEquals("Courage", resource.getFirstName());
-	assertEquals("Cowardly Dog", resource.getSurname());
+        resource = this.repository.findById(resource.getId());
+        assertEquals("0644010101", resource.getContactNumber());
+        assertEquals("junit_test@fixx.it", resource.getEmail());
+        assertEquals("Courage", resource.getFirstName());
+        assertEquals("Cowardly Dog", resource.getSurname());
     }
 
     @Test
     public void deleteTest() {
-	Resource resource = this.repository.findByEmail("junit_test@fixx.it");
-	assertNotNull(resource);
+        Resource resource = this.repository.findByEmail("junit_test@fixx.it");
+        assertNotNull(resource);
 
-	this.repository.findById(resource.getId());
-	this.repository.delete(resource);
+        this.repository.findById(resource.getId());
+        this.repository.delete(resource);
 
-	assertNull(this.repository.findById(resource.getId()));
+        assertNull(this.repository.findById(resource.getId()));
     }
 
     @After
     public void cleanup() {
-	while (true) {
-	    Resource resource = this.repository.findByEmail("junit_test@fixx.it");
-	    if (resource != null) {
-		// System.out.println("******************************************deleting:
-		// " + resource.toString());
-		this.repository.delete(resource);
-	    } else {
-		break;
-	    }
-	}
+        while (true) {
+            Resource resource = this.repository.findByEmail("junit_test@fixx.it");
+            if (resource != null) {
+                // System.out.println("******************************************deleting:
+                // " + resource.toString());
+                this.repository.delete(resource);
+            } else {
+                break;
+            }
+        }
     }
 
     /*
