7854b4a7781cc09da17f73e88fd35ab8985ab374
Riaan Schoeman <adriaan.s.schoeman@gmail.com>
2016-11-24 16:15:44 +0100

    added logo, added dropdown feature for creating dropdowns in templates.

--- src/main/java/nl/fixx/asset/data/domain/AssetFieldType.java
+++ src/main/java/nl/fixx/asset/data/domain/AssetFieldType.java
@@ -6,6 +6,10 @@
 package nl.fixx.asset.data.domain;
 
 /**
+ * This class is used to define the field data types. Adding of new data types
+ * can be done with no issue to db but changing the name of the enum class will
+ * break the field link. You can change the string value type as "text" to
+ * "string" with no issues.
  *
  * @author adriaan
  */
@@ -13,16 +17,17 @@ public enum AssetFieldType {
     ASSET_INPUT_INT_TYPE("Number"),
     ASSET_INPUT_STR_TYPE("Text"),
     ASSET_INPUT_DAT_TYPE("Date"),
-    ASSET_INPUT_MON_TYPE("Monetary");
+    ASSET_INPUT_MON_TYPE("Monetary"),
+    ASSET_INPUT_DRD_TYPE("Dropdown");
 
     private final String type;
 
     private AssetFieldType(String type) {
-	this.type = type;
+        this.type = type;
     }
 
     public String getType() {
-	return type;
+        return type;
     }
 
 }
