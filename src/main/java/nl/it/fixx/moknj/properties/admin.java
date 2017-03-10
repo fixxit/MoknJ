c0f68feba3ac749f6386b7077ca30fb29b03d068
Adriaan <Adriaan@MSI>
2017-03-10 15:00:59 +0100

    refactoring

---
+++ src/main/java/nl/it/fixx/moknj/properties/admin.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.it.fixx.moknj.properties;
+
+/**
+ *
+ * @author Adriaan
+ */
+public class admin {
+
+    public static final String CLASSPATH = "classpath:admin.properties";
+    public static final String USER = "admin.user";
+    public static final String PASSWORD = "admin.pass";
+}
